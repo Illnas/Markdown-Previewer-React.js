39db6f3250c721bb218af53084e804fdca7dc215
Nadatari <dariosting@gmail.com>
2021-06-11 18:33:39 +0200

    Project is finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
+import { useState } from "react";
+import marked from "marked";
 
 
+
+marked.setOptions({
+  gfm: true,
+  breaks: true
+});
 
 
 function App() {
+  const [value, setValue] = useState(`
+  # Heading
+  ## Sub heading
+  
+  Single line code example \`const a = "Hello"\`
+
+
+
+  Multi line code example:
+  \`\`\`
+    const random = () => {
+      let rng = Math.floor(Math.random() * 1643);
+      return rng;
+    }
+  \`\`\`
+    
+  Bold text example **bold**
+  Link example: [links](https://www.freecodecamp.com)
+  Block quote example"
+   > Blockquote
+ 
+  - List examplee
+
+  ![Image](https://unsplash.com/photos/vm2cwMEiUFA "Image")
+  `
+);
+
+  const collectMarkdown = () => {
+    return {__html: marked(value)};
+  }
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+
+  
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="main-container">
+      <h1 className="heading">Markdown Previewer</h1>
+      <div className="container">
+      <textarea id="editor"  value={value} onChange={onChange}></textarea>
+      <div id="preview" dangerouslySetInnerHTML={collectMarkdown()}></div>
+      </div>
+      
     </div>
   );
 }
